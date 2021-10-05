dc7092b7e191ad66f807970dc806b8b3383a1145
Ramitphi <18ucs144@lnmiit.ac.in>
2021-10-05 13:25:53 +0530

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,18 +2,257 @@ import logo from "./logo.svg";
 import { useEffect, useState } from "react";
 import Web3 from "web3";
 import { Biconomy } from "@biconomy/mexa";
+const BigNumber = require("bignumber.js");
 
 function App() {
-  const biconomy = new Biconomy(window.ethereum, {
-    apiKey: "q9oEztJM8.e8ed08a7-5b38-48e3-b4c0-f66e6b66f407",
-    debug: true,
-  });
-
-  const web3 = new Web3(biconomy);
+  const tokenabi = [
+    {
+      inputs: [],
+      name: "name",
+      outputs: [
+        {
+          name: "",
+          type: "string",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      constant: false,
+      inputs: [
+        {
+          name: "_spender",
+          type: "address",
+        },
+        {
+          name: "_value",
+          type: "uint256",
+        },
+      ],
+      name: "approve",
+      outputs: [
+        {
+          name: "",
+          type: "bool",
+        },
+      ],
+      payable: false,
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: "totalSupply",
+      outputs: [
+        {
+          name: "",
+          type: "uint256",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      constant: false,
+      inputs: [
+        {
+          name: "_from",
+          type: "address",
+        },
+        {
+          name: "_to",
+          type: "address",
+        },
+        {
+          name: "_value",
+          type: "uint256",
+        },
+      ],
+      name: "transferFrom",
+      outputs: [
+        {
+          name: "",
+          type: "bool",
+        },
+      ],
+      payable: false,
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: "decimals",
+      outputs: [
+        {
+          name: "",
+          type: "uint8",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      constant: true,
+      inputs: [
+        {
+          name: "_owner",
+          type: "address",
+        },
+      ],
+      name: "balanceOf",
+      outputs: [
+        {
+          name: "balance",
+          type: "uint256",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: "symbol",
+      outputs: [
+        {
+          name: "",
+          type: "string",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      constant: false,
+      inputs: [
+        {
+          name: "_to",
+          type: "address",
+        },
+        {
+          name: "_value",
+          type: "uint256",
+        },
+      ],
+      name: "transfer",
+      outputs: [
+        {
+          name: "",
+          type: "bool",
+        },
+      ],
+      payable: false,
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      constant: true,
+      inputs: [
+        {
+          name: "_owner",
+          type: "address",
+        },
+        {
+          name: "_spender",
+          type: "address",
+        },
+      ],
+      name: "allowance",
+      outputs: [
+        {
+          name: "",
+          type: "uint256",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      payable: true,
+      stateMutability: "payable",
+      type: "fallback",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          name: "owner",
+          type: "address",
+        },
+        {
+          indexed: true,
+          name: "spender",
+          type: "address",
+        },
+        {
+          indexed: false,
+          name: "value",
+          type: "uint256",
+        },
+      ],
+      name: "Approval",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          name: "from",
+          type: "address",
+        },
+        {
+          indexed: true,
+          name: "to",
+          type: "address",
+        },
+        {
+          indexed: false,
+          name: "value",
+          type: "uint256",
+        },
+      ],
+      name: "Transfer",
+      type: "event",
+    },
+  ];
 
   const abi = [
     {
       inputs: [
+        {
+          internalType: "address",
+          name: "to_",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "amount_",
+          type: "uint256",
+        },
+      ],
+      name: "transfertip",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "token",
+          type: "address",
+        },
         {
           internalType: "address",
           name: "_trustedForwarder",
@@ -43,24 +282,6 @@ function App() {
       type: "function",
     },
     {
-      inputs: [
-        {
-          internalType: "address",
-          name: "to_",
-          type: "address",
-        },
-        {
-          internalType: "uint256",
-          name: "amount_",
-          type: "uint256",
-        },
-      ],
-      name: "transfertip",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
       inputs: [],
       name: "trustedForwarder",
       outputs: [
@@ -87,59 +308,109 @@ function App() {
       type: "function",
     },
   ];
-  const caddress = "0xBFeFFB821DbA87dff2f3E018Ab2C6d1cF5F60aD5";
+
+  const [amt, setamt] = useState(null);
+
+  const biconomy = new Biconomy(window.ethereum, {
+    apiKey: "api -key",
+    debug: true,
+  });
+
+  const BigNumber = require("bignumber.js");
+  const caddress = "0x2C6a4a676CA7E6e9bC70BD6530968599645e17B7";
+  const laddress = "0xa36085F69e2889c224210F603D836748e7dC0088";
   var contract;
+  var linkcontract;
   const usraddress = "0xCAa931a56cCbF30B82CED72604DdC7182964bB71";
   const recadd = "0x905040585A59C5B0E83Be2b247fC15a81FF4E533";
+
+  const web3 = new Web3(biconomy);
+
   let accounts;
+  async function connect() {
+    window.ethereum.enable();
+    accounts = await web3.eth.getAccounts();
+    console.log(accounts[0]);
+  }
+
+  // useEffect => {
+
+  // }
 
   biconomy
     .onEvent(biconomy.READY, () => {
-      // Initialize your dapp here like getting user accounts etc
+      contract = new web3.eth.Contract(abi, caddress);
+      console.log(contract);
+      linkcontract = new web3.eth.Contract(tokenabi, laddress);
+      console.log(linkcontract);
+      console.log(biconomy);
     })
     .onEvent(biconomy.ERROR, (error, message) => {
       // Handle error while initializing mexa
       console.log(error);
     });
 
-  window.ethereum.request({ method: "eth_requestAccounts" });
-  accounts = web3.eth.getAccounts();
+  function samt(event) {
+    setamt(event.target.value);
+    console.log(event.target.value);
+  }
 
-  contract = new web3.eth.Contract(abi, caddress);
+  let oneEther = new BigNumber(1);
+  oneEther = oneEther.shiftedBy(18);
+  console.log(oneEther);
 
-  console.log("signing txn");
-  let tx = contract.methods.transfertip(recadd, 10).send({
-    from: usraddress,
-    signatureType: biconomy.EIP712_SIGN,
-    //optionally you can add other options like gasLimit
-  });
+  async function apptxn() {
+    console.log("signing txn");
 
-  console.log("tx signed");
-  tx.on("transactionHash", function (hash) {
-    console.log(`Transaction hash is ${hash}`);
-  }).once("confirmation", function (confirmationNumber, receipt) {
-    console.log(receipt);
-    console.log(receipt.transactionHash);
-    //do something with transaction hash
-  });
+    let txn = linkcontract.methods.approve(caddress, oneEther).send({
+      from: accounts[0],
+      signatureType: biconomy.EIP712_SIGN,
+      //optionally you can add other options like gasLimit
+    });
 
+    txn
+      .on("transactionHash", function (hash) {
+        console.log(`Transaction hash is ${hash}`);
+        //showInfoMessage(`Transaction sent. Waiting for confirmation ..`);
+      })
+      .once("confirmation", function (confirmationNumber, receipt) {
+        console.log(receipt);
+        console.log(receipt.transactionHash);
+        //do something with transaction hash
+      });
+  }
+
+  async function sendtxn() {
+    let tx = contract.methods.transfertip(recadd, oneEther).send({
+      from: accounts[0],
+      signatureType: biconomy.EIP712_SIGN,
+      //optionally you can add other options like gasLimit
+    });
+
+    tx.on("transactionHash", function (hash) {
+      console.log(`Transaction hash is ${hash}`);
+      //showInfoMessage(`Transaction sent. Waiting for confirmation ..`);
+    }).once("confirmation", function (confirmationNumber, receipt) {
+      console.log(receipt);
+      console.log(receipt.transactionHash);
+      //do something with transaction hash
+    });
+  }
   return (
     <div className="App">
       NFT Gallery
-      {/*       
-            <div className ="gallery">
-               {
-                 imgdata.map((item,index) =>{
-                   return(
-                     <div className="pics" key={index}>
-                      <a href = {item.link}>
-                      <img src = {item.img} />
-                      </a>
-                     </div>
-                   )
-                 })
-               } 
-            </div> */}
+      {/* <button onClick={load}>load contract</button> */}
+      <br></br> <br></br>
+      <button onClick={connect}>connect</button>
+      {/* <br></br> <br></br>
+        <input type="text" onClick={samt} />
+        <br></br>
+        {/* <button onClick={submitaddress}>Submit Address</button>
+        <br></br> */}
+      <button onClick={apptxn}>Approve Txn</button>
+      <br></br>
+      <br></br>
+      <button onClick={sendtxn}>send Txn</button>
     </div>
   );
 }
